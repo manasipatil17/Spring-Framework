f9de437bd6db2b04f663636d30c3ffce1596fc99
Manasi Patil <manasipatil17052004@gmail.com>
2025-08-06 22:58:40 +0530

    added sim interface and user airtel and jio classes

---
+++ Day3_AnnoQualifier/src/main/java/com/basePakage/Jio.java
@@ -0,0 +1,14 @@
+package com.basePakage;
+
+import org.springframework.stereotype.Component;
+
+@Component()
+public class Jio implements Sim{
+
+	@Override
+	public String calling() {
+		// TODO Auto-generated method stub
+		return "Calling from Jio";
+	}
+
+}
---
+++ Day3_AnnoQualifier/src/main/java/com/basePakage/User.java
@@ -0,0 +1,12 @@
+package com.basePakage;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+	
+	private Sim sim1;
+	private Sim sim2;
+	
+}
